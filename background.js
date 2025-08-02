fd4142db048612ec21d4b838145f3389d2105d70
Irdi Kuka <a12407104@univie.ac.at>
2025-08-02 19:36:39 +0200

    Pushed

--- background.js
+++ background.js
@@ -1,17 +1,29 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "navigateToCoordinates") {
+chrome.runtime.onMessage.addListener((request, sender) => {
+  if (request.action === "navigateToCoordinates" && sender.tab) {
     chrome.scripting.executeScript({
       target: { tabId: sender.tab.id },
+      world: 'MAIN',
       func: (x, y) => {
-        if (typeof picassio !== 'undefined' && picassio.map && picassio.map.sc) {
-          picassio.map.sc.goTo(x, y);
-        }
+        const tryGo = () => {
+          if (typeof picassio !== 'undefined' && picassio.map && picassio.map.sc) {
+            picassio.map.sc.goTo(x, y);
+          } else {
+            setTimeout(tryGo, 300);
+          }
+        };
+
+        
+
+        tryGo();
         if (typeof xajax_switchVillageToGlobalMap === 'function') {
-          xajax_switchVillageToGlobalMap();
+          setTimeout(xajax_switchVillageToGlobalMap(), 300);
         }
       },
       args: [request.x, request.y]
+    }, () => {
+      if (chrome.runtime.lastError) {
+        console.error('Imperia Spy navigation error:', chrome.runtime.lastError);
+      }
     });
-    return true;
   }
 });
